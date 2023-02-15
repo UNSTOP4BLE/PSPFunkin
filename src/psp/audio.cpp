619f02d384a1a8ba27d97722f7e467a0181614bb
UNSTOP4BLE <88965856+UNSTOP4BLE@users.noreply.github.com>
2023-02-15 01:35:52 +0100

    more audio stuff

--- src/psp/audio.cpp
+++ src/psp/audio.cpp
@@ -25,53 +25,33 @@ struct __attribute__((packed))FMT
 	uint16_t channels;
 	uint32_t samplerate;
 	uint32_t byterate;
-	uint16_t allign;
+	uint16_t align;
 	uint16_t bps;
+};
+
+class AudioBuffer {
+public:
+
+    inline int getNumSamples(void) {
+        return data.size() / (channels * SDL_AUDIO_BITSIZE(format) / 8);
+    }
+    std::vector<uint8_t> data; 
+    uint16_t channels;
+    uint32_t samplerate;
+    SDL_AudioFormat format;
 };
 
 class WAVStreamSource
 {
 public:
-	inline WAVStreamSource(const char *path) {
-		wavFile = fopen(path, "rb");
+	WAVStreamSource(const char *path);
 
-		if (wavFile == NULL)
-		{
-			ErrMSG("FAILED TO FIND WAV FILE AT PATH %s", path);
-			close();
-			return;
-		}
+    inline int getPosition(void)
+    {
+        return (ftell(wavFile) - dataOffset) / (fmt.channels * SDL_AUDIO_BITSIZE(format) / 8);
+    }
 
-		assert(_readValue<uint32_t>(wavFile) == "RIFF"_m);
-		_readValue<uint32_t>(wavFile); //ignore total file size
-		assert(_readValue<uint32_t>(wavFile) == "WAVE"_m);
-
-		//if (_readValue<uint32_t>(wavFile) != ("RIFF"_m || "WAVE"_m))
-		//{
-		//	ErrMSG("INVALID WAV FILE");
-		//	close();
-	//		return;
-	//	}  
-		uint32_t chunkType = _readValue<uint32_t>(wavFile);
-		uint32_t chunkLength = _readValue<uint32_t>(wavFile);
-
-		while (!feof(wavFile))
-		{
-			if (chunkType == "fmt "_m)
-			{
-				_readValueInPlace(wavFile, fmt);
-				assert(fmt.format == 1); // 1 is PCM, 0x11 is ADPCM
-				assert(fmt.bps == 16);
-			}
-			else if (chunkType == "data"_m) {
-				dataOffset = ftell(wavFile);    
-				dataLength = chunkLength;
-				return;
-			}  
-			else
-				fseek(wavFile, chunkLength, SEEK_CUR);
-		}
-	}
+    int readBuf(AudioBuffer &buf, int numSamples);
 
 	inline ~WAVStreamSource(void) {
 		fclose(wavFile);
@@ -80,16 +60,59 @@ private:
 	FILE *wavFile;
 	FMT fmt;
 	int dataOffset;
-	uint32_t dataLength;
+	uint32_t totalNumSamples;
+    SDL_AudioFormat format;
 };
 
-class AudioBuffer {
-public:
-	uint16_t *data;
-	uint16_t channels;
-	uint32_t samplerate;
-	uint32_t dataLength;
-private:
+WAVStreamSource::WAVStreamSource(const char *path)
+{
+    wavFile = fopen(path, "rb");
 
+    if (wavFile == NULL)
+    {
+        ErrMSG("FAILED TO FIND WAV FILE AT PATH %s", path);
+        close();
+        return;
+    }
 
-};
+    assert(_readValue<uint32_t>(wavFile) == "RIFF"_m);
+    _readValue<uint32_t>(wavFile); //ignore total file size
+    assert(_readValue<uint32_t>(wavFile) == "WAVE"_m);
+
+        //if (_readValue<uint32_t>(wavFile) != ("RIFF"_m || "WAVE"_m))
+        //{
+        //  ErrMSG("INVALID WAV FILE");
+        //  close();
+    //      return;
+    //  }  
+    uint32_t chunkType = _readValue<uint32_t>(wavFile);
+    uint32_t chunkLength = _readValue<uint32_t>(wavFile);
+
+    while (!feof(wavFile))
+    {
+        if (chunkType == "fmt "_m)
+        {
+            _readValueInPlace(wavFile, fmt);
+            assert(fmt.format == 1); // 1 is PCM, 0x11 is ADPCM
+            assert(fmt.bps == 16);
+        }
+        else if (chunkType == "data"_m) {
+            dataOffset = ftell(wavFile);    
+            totalNumSamples = chunkLength / (fmt.channels * sizeof(uint16_t));
+            return;
+        }  
+        else
+            fseek(wavFile, chunkLength, SEEK_CUR);
+    }
+}
+
+int WAVStreamSource::readBuf(AudioBuffer &buf, int numSamples) {
+    int actualNumSamples = std::min(numSamples, totalNumSamples - getPosition());
+    if (!actualNumSamples) return 0;
+
+    buf.data.resize(actualNumSamples * fmt.channels);
+    buf.channels = fmt.channels;
+    buf.samplerate = fmt.samplerate;
+
+    return fread(buf.data.data(), fmt.channels * sizeof(uint16_t), actualNumSamples, wavFile);
+}
